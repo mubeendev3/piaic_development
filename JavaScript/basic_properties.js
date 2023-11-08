f70d8453dc62553a8260fa291b3929ca1b5fe8cb
Mubeen Mehmood <mubeendev3@gmail.com>
2023-11-08 21:13:33 +0500

    TypeOf operator

--- JavaScript/basic_properties.js
+++ JavaScript/basic_properties.js
@@ -2,5 +2,10 @@
 // console.log("Hello World!");
 // document.write("JavaScript>>> Hello World!");
 // var firstName; // Variable declaration 
-// firstName = "Mubeen"; // Variable initialization
+firstName = "Mubeen"; // Variable initialization
+age = 20;
+isFeePaid = true;
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isFeePaid);
 
