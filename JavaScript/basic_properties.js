781076518f6b39c0a80b0d66ba36a6121c685466
Mubeen Mehmood <mubeendev3@gmail.com>
2023-11-08 21:12:50 +0500

    TypeOf operator

--- JavaScript/basic_properties.js
+++ JavaScript/basic_properties.js
@@ -1,3 +1,6 @@
 // alert("Hello World!");
 // console.log("Hello World!");
-document.write("JavaScript>>> Hello World!");
+// document.write("JavaScript>>> Hello World!");
+// var firstName; // Variable declaration 
+// firstName = "Mubeen"; // Variable initialization
+
